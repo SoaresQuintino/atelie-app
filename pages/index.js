18da476749825be0ac09ba76bb7b8d77464efe86
Soares Quintino <soares@192.168.1.21>
2024-07-25 22:45:21 -0300

    fix:Atualizar componentes e página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,54 @@
-import React from 'react';
-import Head from 'next/head';
-import Topbar from '../components/Topbar';
-import GridItem from '../components/GridItem';
+/* eslint-disable @next/next/no-css-tags */
+import React from "react";
+import Head from "next/head";
+import Topbar from "../components/Topbar";
+import GridItem from "../components/GridItem";
+import Cover from "../components/Cover";
+import CollectionCard from "../components/CollectionCard";
 
-export default function HomePage({produtos}) {
-
-
-  return (<div className="container mx-auto px-4 py-8">
-   <Head>
+export default function HomePage({ produtos }) {
+  return (
+    <div className="">
+      <Head>
         <title>Ateliê Bela Arte</title>
-        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" />
-   </Head>
-     
-   <Topbar/>
-      
-   <hr className="my-8 border-gray-300" />
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-     
-     {produtos.map((produto) => (
-      
-        // eslint-disable-next-line react/jsx-key
-        <GridItem id={produto.id} src={produto.urlimagem} alt="imagem" title={produto.nome} description={produto.descricao}></GridItem>
-     ))}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+        />
+        <link rel="stylesheet" href="/css/style.css" />
+      </Head>
 
+      <Topbar />
+      <Cover />
+      <CollectionCard />
+
+      <hr className="my-8 border-transparent" />
+
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 md-margin-lr-5 lg-margin-lr-20">
+        {produtos.map((produto) => (
+          // eslint-disable-next-line react/jsx-key
+          <GridItem
+            id={produto.id}
+            src={produto.urlimagem}
+            alt="imagem"
+            title={produto.nome}
+            description={produto.descricao}
+          ></GridItem>
+        ))}
       </section>
-     
-   </div>)
+    </div>
+  );
 }
 
 export async function getServerSideProps() {
-   const res = await fetch('http://localhost:8000/api/produtos');
-   const data = await res.json();
-  
-   const produtos = data['hydra:member'] ;
- 
- 
-   return {
-     props: {
-       produtos,
-     },
-   };
- }
+  const res = await fetch("http://localhost:8000/api/produtos");
+  const data = await res.json();
+
+  const produtos = data["hydra:member"];
+
+  return {
+    props: {
+      produtos,
+    },
+  };
+}
